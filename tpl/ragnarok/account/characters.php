c673520b092c5bcccb0e453da6238ceb5b340058
Eshe <gabriela.wailly@gmail.com>
2014-03-04 17:44:09 -0300

    Fixed character sorting layout.

--- tpl/ragnarok/account/characters.php
+++ tpl/ragnarok/account/characters.php
@@ -8,16 +8,25 @@ use Aqua\Core\App;
  * @var $page         \Page\Main\Ragnarok\Account
  */
 assert('isset($characters) && isset($page) && isset($charmap) && isset($server)');
-$multi_srv = ($server->charmapCount > 1 ? 1 : 0);
 $page->theme->head->enqueueScript(ScriptManager::script('jquery-ui'));
 ?>
 <form method="POST">
 	<input type="hidden" name="selected-server" value="<?php echo $charmap->key?>">
-	<table class="ac-table" id="ac-sort-characters">
+	<table class="ac-table" id="sort-characters" style="table-layout: fixed">
+		<colgroup>
+			<col style="width: 25px">
+			<col style="width: 75px">
+			<col>
+			<col>
+			<col>
+			<col>
+			<col style="width: 40px">
+			<col>
+		</colgroup>
 		<thead>
 		<?php if($server->charmapCount > 1) : ?>
 			<tr class="ac-script">
-				<td colspan="9" style="text-align: right">
+				<td colspan="8" style="text-align: right">
 					<select onchange="document.location.href = this.options[this.selectedIndex].value;" class="ac-script">
 						<?php $x_base_url = App::request()->uri->url(array( 'arguments' => array( '' ) )); ?>
 						<?php foreach($server->charmap as &$cm) : ?>
@@ -29,7 +38,6 @@ $page->theme->head->enqueueScript(ScriptManager::script('jquery-ui'));
 		<?php endif; ?>
 		<tr class="alt">
 			<td></td>
-			<td class="ac-char-slot"><noscript><?php echo __('ragnarok', 'slot')?></noscript></td>
 			<td></td>
 			<td><?php echo __('ragnarok', 'name')?></td>
 			<td><?php echo __('ragnarok', 'class')?></td>
@@ -41,78 +49,74 @@ $page->theme->head->enqueueScript(ScriptManager::script('jquery-ui'));
 		<tbody>
 		<?php if(empty($characters)) : ?>
 			<tr>
-				<td colspan="9" style="text-align: center; font-style: italic;"><?php echo __('application', 'no-search-results')?></td>
+				<td colspan="8" class="ac-table-no-result"><?php echo __('application', 'no-search-results')?></td>
 			</tr>
 		<?php else : foreach($characters as $char) : ?>
 			<?php $name = htmlspecialchars($char->name); ?>
 			<tr class="ac-character">
-				<td class="ac-char-status ac-char-<?php echo ($char->online ? 'online' : 'offline')?>"></td>
-				<td class="ac-char-slot">
-					<input type="hidden" name="<?php echo $char->id?>-slot" value="<?php echo $char->slot?>">
-					<noscript>
-						<input type="number" name="<?php echo $char->id?>-slot" value="<?php echo $char->slot?>">
-					</noscript>
+				<td class="ac-char-status ac-char-<?php echo ($char->online ? 'online' : 'offline')?>">
+					<input type="hidden" class="slot" name="<?php echo $char->id?>-slot" value="<?php echo $char->slot?>">
 				</td>
-				<td class="ac-char-head"><img src="<?php echo ac_char_head($char, true)?>"></td>
-				<td class="ac-char-name"><a href="<?php echo $char->url()?>"><?php echo htmlspecialchars($name)?></a></td>
-				<td class="ac-char-class"><?php echo $char->job()?></td>
-				<td class="ac-char-blvl"><?php echo $char->baseLevel?></td>
-				<td class="ac-char-jlvl"><?php echo $char->jobLevel?></td>
-				<td class="ac-char-guild-emblem">
-					<?php if($char->guildId) : ?>
+				<td><img src="<?php echo ac_char_head($char, true)?>"></td>
+				<td><a href="<?php echo $char->url()?>"><?php echo htmlspecialchars($name)?></a></td>
+				<td><?php echo $char->job()?></td>
+				<td><?php echo $char->baseLevel?></td>
+				<td><?php echo $char->jobLevel?></td>
+				<?php if($char->guildId) : ?>
+					<td>
 						<img src="<?php echo ac_guild_emblem(
 							$char->charmap->server->key,
 							$char->charmap->key,
 							$char->guildId
 						)?>">
-					<?php endif; ?>
-				</td>
-				<td class="ac-char-guild-name"><?php echo htmlspecialchars($char->guildName)?></td>
-				<?php if($multi_srv) : ?>
-					<td class="ac-char-server"><?php echo htmlspecialchars($char->charmap->name)?></td>
+					</td>
+					<td><?php echo htmlspecialchars($char->guildName)?></td>
+				<?php else : ?>
+					<td colspan="2"></td>
 				<?php endif; ?>
 			</tr>
 		<?php endforeach; endif; ?>
 		</tbody>
 		<tfoot>
-		<tr>
-			<td colspan="9">
-				<div class="ac-script" style="float: left; line-height: 2em; font-size: 0.9em">
-					<?php echo __('ragnarok', 'drag-drop-sort')?>
-				</div>
-				<input class="ac-button" type="submit" name="x-change-slot" value="<?php echo __('ragnarok', 'change-slot')?>">
-			</td>
-		</tr>
+			<tr>
+				<td colspan="8">
+					<div class="ac-script" style="float: left; line-height: 2em; font-size: 0.9em">
+						<?php echo __('ragnarok', 'drag-drop-sort')?>
+					</div>
+					<input class="ac-button" type="submit" name="x-change-slot" value="<?php echo __('ragnarok', 'change-slot')?>">
+				</td>
+			</tr>
 		</tfoot>
 	</table>
 </form>
+<span class="ac-search-result"><?php echo __('application',
+                                             'search-results-' . (count($characters) === 1 ? 's' : 'p'),
+                                             number_format(count($characters)))?></span>
 <?php if (!empty($characters)) : ?>
 <script>
-	var table = $("#ac-sort-characters");
-	$("tbody", table).sortable({
+(function($) {
+	$("#sort-characters tbody").sortable({
 		helper: function(e, ui) {
-			$("td", table).each(function() {
-				$(this).width($(this).width());
+			var element = ui.clone(),
+				children = ui.children();
+			element.children().each(function(i) {
+				$(this).width(children.eq(i).width());
 			});
-			ui.children().each(function() {
-				$(this).width($(this).width());
-			});
-			return ui;
+			return element;
 		},
-		start: function() {
-			$(this).css("position", "relative");
-		},
-		stop: function() {
-			$(this).css("position", "");
-		},
-		update: function(event, ui) {
+		update: function() {
 			$("tr.ac-character", this).each(function(i) {
-				$(this).find('.ac-char-slot input').attr('value', i + 1);
+				$(this).find('input.slot').attr('value', i + 1);
 			});
 		},
-		snap: true,
+		containment: "parent",
+		tolerance: "pointer",
+		forceHelperSize: true,
+		forcePlaceholderSize: true,
+		snap: false,
+		axis: "y",
 		cursor: "move"
 	});
+})(jQuery);
 </script>
 <?php endif; ?>
-<span class="ac-search-result"><?php echo __('application', 'search-results-' . (count($characters) === 1 ? 's' : 'p'), number_format(count($characters)))?></span>
