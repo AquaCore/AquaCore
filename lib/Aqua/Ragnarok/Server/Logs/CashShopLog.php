ae7ec505de3670bb13c599609a9bb965818edd94
Eshe <gabriela.wailly@gmail.com>
2014-03-31 18:20:39 -0300

    Added "username" and "amount" rows to ac_cash_shop_log, added view-server-acc permission.

--- lib/Aqua/Ragnarok/Server/Logs/CashShopLog.php
+++ lib/Aqua/Ragnarok/Server/Logs/CashShopLog.php
@@ -1,14 +1,91 @@
 <?php
-
 namespace Aqua\Ragnarok\Server\Logs;
 
 class CashShopLog
 {
-
-	public $id;
+	/**
+	 * @var \Aqua\Ragnarok\Server\CharMap
+	 */
 	public $charmap;
+	/**
+	 * Log ID
+	 *
+	 * @var int
+	 */
+	public $id;
+	/**
+	 * Request IP address
+	 *
+	 * @var string
+	 */
 	public $ipAddress;
+	/**
+	 * Target server account ID
+	 *
+	 * @var int
+	 */
 	public $accountId;
+	/**
+	 * Target server account name
+	 *
+	 * @var string
+	 */
+	public $username;
+	/**
+	 * Purchase timestamp
+	 *
+	 * @var int
+	 */
 	public $date;
-	public $cart;
+	/**
+	 * Total price
+	 *
+	 * @var int
+	 */
+	public $total;
+	/**
+	 * Amount of distinct items purchased
+	 *
+	 * @var int
+	 */
+	public $items;
+	/**
+	 * Amount of items purchased
+	 *
+	 * @var int
+	 */
+	public $amount;
+	/**
+	 * @var array
+	 */
+	protected $_cart;
+
+	/**
+	 * Format purchase date
+	 *
+	 * @param string $format
+	 * @return string
+	 */
+	public function date($format)
+	{
+		return strftime($format, $this->date);
+	}
+
+	public function cart()
+	{
+		if(!$this->_cart) {
+			$sth = $this->charmap->log->connection()->prepare("
+			SELECT `item_id`, `amount`, `item_price` AS `price`
+			FROM {$this->charmap->log->table('ac_cash_shop_items')}
+			WHERE id = ?
+			");
+			$sth->bindValue(1, $this->id, \PDO::PARAM_INT);
+			$sth->execute();
+			$this->_cart = array();
+			while($data = $sth->fetch(\PDO::FETCH_ASSOC)) {
+				$this->_cart[$data['item_id']] = $data;
+			}
+		}
+		return $this->_cart;
+	}
 }
--- schema/charmap-log/ac_cash_shop_log.sql
+++ schema/charmap-log/ac_cash_shop_log.sql
@@ -2,8 +2,10 @@ CREATE TABLE IF NOT EXISTS `ac_cash_shop_log` (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   ip_address VARCHAR(46) NOT NULL,
   account_id INT UNSIGNED NOT NULL,
+  username VARCHAR(23) NOT NULL,
   total INT UNSIGNED NOT NULL,
   items SMALLINT UNSIGNED NOT NULL,
+  amount SMALLINT UNSIGNED NOT NULL,
   `date` DATETIME NOT NULL,
   PRIMARY KEY ( id ),
   INDEX `_ac_cash_shop_log__account_id_IN` ( account_id )
