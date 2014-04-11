3d289d9f7b6a7ead3493a51cfa09baf73b428b60
Eshe <gabriela.wailly@gmail.com>
2014-04-11 01:27:09 -0300

    Added lua and update-lang command line scripts

---
+++ cli/lua.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Update settings/chargen/hats.php based on the game lua files
+ * Usage: php cli/lua.php <directory>
+ *
+ * @example php cli/lua.php C:/Path/To/Lua/Files
+ */
+
+define('Aqua\ROOT',        str_replace('\\', '/', rtrim(basename(__DIR__), DIRECTORY_SEPARATOR)));
+define('Aqua\SCRIPT_NAME', basename(__FILE__));
+define('Aqua\ENVIRONMENT', 'CLI');
+define('Aqua\PROFILE',     'CHARGEN');
+
+if(!isset($argv[0]) || !($dir = $argv[0])) {
+	$dir = getcwd() ?: __DIR__;
+}
+
+if(!is_dir($dir)) {
+	die("$dir is not a directory.\r\n");
+}
+if(!is_readable($dir)) {
+	die("$dir is nto readable.\r\n");
+}
+$luaFiles = array(
+	'accessoryid' => "$dir/datainfo/accessoryid.lua",
+	'accname' => "$dir/datainfo/accname.lua",
+);
+
+foreach($luaFiles as $lua => &$file) {
+	if(array_key_exists($lua, $argl)) {
+		$file = $argl[$lua];
+	}
+	if(!file_exists($file)) {
+		echo "$lua.lua not found.\r\n";
+	} else if(!is_readable($file)) {
+		echo "$lua.lua is not readable.\r\n";
+	} else {
+		echo "$lua.lua found at $file.\r\n";
+		continue;
+	}
+	$file = null;
+}
+
+if($luaFiles['accname'] && $luaFiles['accessoryid']) {
+	echo "Parsing accname.lua and accessoryid.lua...\r\n";
+	$accessoryId = file_get_contents($luaFiles['accessoryid']);
+	$accName     = file_get_contents($luaFiles['accname']);
+	$pattern     = '/ACCESSORY_([^\s=]+)\s*=\s*(\d+)/Sim';
+	$offset      = 0;
+	$viewIds     = array();
+	while(preg_match($pattern, $accessoryId, $match, PREG_OFFSET_CAPTURE, $offset)) {
+		$offset = $match[2][1];
+		$view = intval($match[2][0]);
+		$name = preg_quote($match[1][0], '/');
+		if(preg_match('/\[ACCESSORY_IDs\.ACCESSORY_' . $name . '\]\s*=\s*"([^"]+)"/im', $accName, $match)) {
+			$viewIds[$view] = '';
+			$len = strlen($match[1]);
+			for($i = 1; $i < $len; ++$i) {
+				$viewIds[$view].= '\\x' . dechex(ord($match[1][$i]));
+			}
+		}
+	}
+	$out = "<?php\r\n return array(\r\n";
+	foreach($viewIds as $id => $str) {
+		$out.= "\t$id => \"$str\",\r\n";
+	}
+	$out = substr($out, 0, -3);
+	$out.= "\r\n);\r\n";
+	file_put_contents(\Aqua\ROOT . '/settings/chargen/hats.php', $out);
+	echo count($viewIds) . " view ids found.\r\n";
+}
---
+++ cli/update-lang.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Replace translations in the database with up to date ones found in /install/language xml files
+ * Usage: php cli/update-lang.php [namespace=<namespace>,<namespace>,...] [code=<language code>]
+ *
+ * @example php cli/update-lang.php namespace=account,ragnarok code=en Replaces the english translations of the ragnarok and account namespaces
+ *          php cli/update-lang.php                                    Replaces all translations
+ */
+
+define('Aqua\ROOT',        str_replace('\\', '/', rtrim(dirname(__DIR__), DIRECTORY_SEPARATOR)));
+define('Aqua\SCRIPT_NAME', basename(__FILE__));
+define('Aqua\ENVIRONMENT', 'CLI');
+define('Aqua\PROFILE',     'CHARGEN');
+
+include __DIR__ . '/../lib/bootstrap.php';
+
+if(!array_key_exists('code', $argl)) {
+	$code = 'en';
+} else {
+	$code = $argl['code'];
+}
+
+$path = \Aqua\ROOT . "/install/language/$code/namespaces";
+if(!isset($argl['namespace'])) {
+	$namespaces = glob("$path/*.xml");
+} else {
+	$namespaces = array();
+	$x = explode(',', $argl['namespace']);
+	foreach($x as $name) {
+		if(file_exists("$path/$name.xml")) {
+			$namespaces[] = "$path/$name.xml";
+		}
+	}
+}
+
+if(empty($namespaces)) {
+	echo "Nothing to import.\r\n";
+	die;
+}
+
+$tbl = ac_table('language_words');
+$lang = \Aqua\Core\L10n::get($code);
+if(!$lang) {
+	echo "Language \"$code\" does not exist, cannot insert translations.\r\n";
+	die;
+}
+foreach($namespaces as $file) {
+	$name = basename($file, '.xml');
+	echo "Deleting old translations for \"$name\".\r\n";
+	$sth = \Aqua\Core\App::connection()->prepare("
+	DELETE FROM `$tbl`
+	WHERE _language_id = ? AND _namespace = ?
+	");
+	$sth->bindValue(1, $lang->id, PDO::PARAM_INT);
+	$sth->bindValue(2, $name, PDO::PARAM_STR);
+	$sth->execute();
+	echo "Importing namespace \"$name\"...\r\n";
+	$xml = new SimpleXMLElement(file_get_contents($file));
+	\Aqua\Core\L10n::import($xml);
+	echo "Finished importing \"$name\".\r\n";
+}
+
+echo "All namespaces imported!\r\n";
