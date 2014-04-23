48a3ecc99e334c79454904d62f09712d77648340
Eshe <gabriela.wailly@gmail.com>
2014-04-23 02:16:30 -0300

    Fixed ragnarok server routes

--- application/routing.php
+++ application/routing.php
@@ -8,6 +8,45 @@ use Aqua\Router\Router;
 
 $router = new Router;
 
+function ac_router_set_active_server($key)
+{
+	return (App::$activeServer = Server::get($key));
+}
+function ac_router_set_active_charmap($key)
+{
+	if(!App::$activeServer) {
+		return false;
+	}
+	if($key === null || (strcasecmp($key, 'server') === 0 && !isset(App::$activeServer->charmap['server']))) {
+		return (App::$activeCharMapServer = App::$activeServer->charmap());
+	} else {
+		return (App::$activeCharMapServer = App::$activeServer->charmap($key));
+	}
+}
+function ac_router_set_active_account($key)
+{
+	if(!App::$activeServer) {
+		return false;
+	}
+	if(App::settings()->get('ragnarok')->get('acc_username_url', false)) {
+		App::$activeRagnarokAccount = App::$activeServer->get($key, 'username');
+	} else {
+		App::$activeRagnarokAccount = App::$activeServer->get($key, 'id');
+	}
+	return (bool)App::$activeRagnarokAccount;
+}
+function ac_router_set_active_char($key) {
+	if(!App::$activeCharMapServer) {
+		return false;
+	}
+	if(App::settings()->get('ragnarok')->get('char_name_url', false)) {
+		App::$activeRagnarokCharacter = App::$activeCharMapServer->character($key, 'name');
+	} else {
+		App::$activeRagnarokCharacter = App::$activeCharMapServer->character($key, 'id');
+	}
+	return (bool)App::$activeRagnarokCharacter;
+}
+
 $router->add('Main Site')
 	->map('/*', '/main/:path');
 $router->add('Pages')
@@ -19,102 +58,91 @@ $router->add('News Tags')
 $router->add('News Categories')
 	->map("/news/category/:category/", '/main/news/action/category/:category');
 $_404 = 'ragnarok/account|ragnarok/server/char';
-if(Server::$serverCount > 0) {
+if(Server::$serverCount > 1) {
 	$servers = implode('|', array_keys(Server::$servers));
 	// /ro/<server>
 	$router->add('Ragnarok Server Name')
 		->map("/ro/:server[$servers]/*", '/main/ragnarok/:path')
-		->attach('parse_ok', function($event, $match, Request $request) {
-			App::$activeServer = Server::get($match['server']);
+		->attach('parse_ok', function($e, $match) {
+			ac_router_set_active_server($match['server']);
 		});
-	// /ro/<server>/a/<username|id>/*
+	// /ro/<server>/a/<account>/*
 	$router->add('Ragnarok Server Name - Account')
 		->map("/ro/:server[$servers]/a/:username/*", '/main/ragnarok/account/:path')
-		->attach('parse_ok', function($event, $match, Request $request) {
-			if(App::$activeServer = Server::get($match['server'])) {
-				App::$activeRagnarokAccount = App::$activeServer->login->get(
-					$match['username'],
-					App::settings()->get('ragnarok')->get('acc_username_url', false) ? 'username' : 'id'
-				);
-			}
+		->attach('parse_ok', function($e, $match) {
+			ac_router_set_active_server($match['server']);
+			ac_router_set_active_account($match['username']);
 		});
 	foreach(Server::$servers as $server) {
-	// /ro/<server>/c/<char server>/*
-		if($server->charmapCount > 1) {
-			$charmap = implode('|', array_keys($server->charmap));
-			$router->add('Ragnarok Server Name (' . $server->key . ') - Char')
-				->map("/ro/:server[{$server->key}]/s/:charmap[$charmap]/*", '/main/ragnarok/server/:path')
-				->attach('parse_ok', function($event, $match, Request $request) {
-					if(App::$activeServer = Server::get($match['server'])) {
-						App::$activeCharMapServer = App::$activeServer->charmap($match['charmap']);
-					}
-				});
-		} else if($server->charmapCount === 1) {
-			$router->add('Ragnarok Server (' . $server->key . ') - Char')
+		if($server->charmapCount === 1) {
+			// /ro/<server>/server/*
+			$router->add('Ragnarok Server (' . $server->key . ') - CharMap')
 			       ->map("/ro/:server[{$server->key}]/server/*", '/main/ragnarok/server/:path')
-			       ->attach('parse_ok', function($event, $match, Request $request) {
-				       if(App::$activeServer = Server::get($match['server'])) {
-					       App::$activeCharMapServer = App::$activeServer->charmap();
-				       }
+			       ->attach('parse_ok', function($e, $match) {
+				       ac_router_set_active_server($match['server']);
+				       ac_router_set_active_charmap(null);
+			       });
+			// /ro/<server>/server/c/<char>/*
+			$router->add('Ragnarok Server (' . $server->key . ') - Char')
+			       ->map("/ro/:server[{$server->key}]/server/c/:char/*", '/main/ragnarok/server/char/:path')
+			       ->attach('parse_ok', function($e, $match) {
+				       ac_router_set_active_server($match['server']);
+				       ac_router_set_active_charmap(null);
+				       ac_router_set_active_char($match['char']);
+			       });
+		} else if($server->charmapCount) {
+			$charmap = implode('|', array_keys($server->charmap)) . '|server';
+			// /ro/<server>/s/<charmap>/*
+			$router->add('Ragnarok Server (' . $server->key . ') - CharMap')
+				->map("/ro/:server[{$server->key}]/s/:charmap[$charmap]/*", '/main/ragnarok/server/:path')
+				->attach('parse_ok', function($e, $match) {
+					ac_router_set_active_server($match['server']);
+					ac_router_set_active_charmap($match['charmap']);
+				});
+			// /ro/<server>/s/<charmap>/c/<char>/*
+			$router->add('Ragnarok Server (' . $server->key . ') - Char')
+			       ->map("/ro/:server[{$server->key}]/s/:charmap[$charmap]/c/:char/*", '/main/ragnarok/server/char/:path')
+			       ->attach('parse_ok', function($e, $match) {
+				       ac_router_set_active_server($match['server']);
+				       ac_router_set_active_charmap($match['charmap']);
+				       ac_router_set_active_char($match['char']);
 			       });
 		}
 	}
 	reset(Server::$servers);
-
-	// Single server
-	if(Server::$serverCount === 1) {
-		App::$activeServer = current(Server::$servers);
-	// /ragnarok/a/<username|id>/*
-		$router->add('Ragnarok - Account')
-			->map("/ragnarok/a/:username/*", '/main/ragnarok/account/:path')
-			->attach('parse_ok', function($event, $match, Request $request) {
-				App::$activeRagnarokAccount = App::$activeServer->login->get(
-					$match['username'],
-					App::settings()->get('ragnarok')->get('acc_username_url', false) ?
-						'username' : 'id'
-				);
+// Single server
+} else if(Server::$serverCount === 1) {
+	App::$activeServer = current(Server::$servers);
+	// /ragnarok/a/<account>/*
+	$router->add('Ragnarok - Account')
+		->map("/ragnarok/a/:username/*", '/main/ragnarok/account/:path')
+		->attach('parse_ok', function($e, $match) {
+			ac_router_set_active_account($match['username']);
+		});
+	// /ragnarok/c/<charmap>/*
+	if(App::$activeServer->charmapCount > 1) {
+		$charmap = implode('|', array_keys(App::$activeServer->charmap));
+		$router->add('Ragnarok - CharMap')
+			->map("/ragnarok/s/:charmap[$charmap]/*", '/main/ragnarok/server/:path')
+			->attach('parse_ok', function($e, $match) {
+				ac_router_set_active_charmap($match['charmap']);
 			});
-	// /ragnarok/c/<char server>/*
-		if(App::$activeServer->charmapCount > 1) {
-			$charmap = implode('|', array_keys(App::$activeServer->charmap));
-			$router->add('Ragnarok - CharMap')
-				->map("/ragnarok/s/:charmap[$charmap]/*", '/main/ragnarok/server/:path')
-				->attach('parse_ok', function($event, $match, Request $request) {
-					if(!(App::$activeCharMapServer = App::$activeServer->charmap($match['charmap'])) &&
-					   $match['charmap'] === 'server') {
-						App::$activeCharMapServer = App::$activeServer->charmap();
-					}
-				});
-			$router->add('Ragnarok - Char')
-				->map("/ragnarok/s/:charmap[$charmap]/c/:char/*", '/main/ragnarok/server/char/:path')
-				->attach('parse_ok', function($event, $match, Request $request) {
-					if(!(App::$activeCharMapServer = App::$activeServer->charmap($match['charmap'])) &&
-					   $match['charmap'] === 'server') {
-						App::$activeCharMapServer = App::$activeServer->charmap();
-					}
-					if(App::$activeCharMapServer) {
-						App::$activeRagnarokCharacter = App::$activeCharMapServer->character(
-							$match['char'],
-							App::settings()->get('ragnarok')->get('char_name_url', false) ?
-								'name' : 'id'
-						);
-					}
-				});
-		} else if(App::$activeServer->charmapCount) {
-			App::$activeCharMapServer = current(App::$activeServer->charmap);
-			$router->add('Ragnarok - Char')
-				->map("/ragnarok/server/c/:char/*", '/main/ragnarok/server/char/:path')
-				->attach('parse_ok', function($event, $match, Request $request) {
-					App::$activeRagnarokCharacter = App::$activeCharMapServer->character(
-						$match['char'],
-						App::settings()->get('ragnarok')->get('char_name_url', false) ?
-							'name' : 'id'
-					);
-				});
-		}
-	} else {
-		$_404 .= '|ragnarok/server';
+		$router->add('Ragnarok - Char')
+			->map("/ragnarok/s/:charmap[$charmap]/c/:char/*", '/main/ragnarok/server/char/:path')
+			->attach('parse_ok', function($e, $match) {
+				ac_router_set_active_charmap($match['charmap']);
+				ac_router_set_active_char($match['char']);
+			});
+	} else if(App::$activeServer->charmapCount) {
+		App::$activeCharMapServer = current(App::$activeServer->charmap);
+		$router->add('Ragnarok - Char')
+			->map("/ragnarok/server/c/:char/*", '/main/ragnarok/server/char/:path')
+			->attach('parse_ok', function($e, $match) {
+				ac_router_set_active_char($match['char']);
+			});
 	}
+} else {
+	$_404 .= '|ragnarok/server';
 }
 
 $router->add('404')
